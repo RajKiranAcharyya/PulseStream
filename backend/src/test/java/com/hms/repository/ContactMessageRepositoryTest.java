3d35cfadda22d31370746eb61a91964f14d1592a
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:13:31 +0530

    test: add setup and mocks for ContactMessageRepositoryTest

---
+++ backend/src/test/java/com/hms/repository/ContactMessageRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.hms.repository;
+
+import com.hms.entity.ContactMessage;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class ContactMessageRepositoryTest {
+
+}
