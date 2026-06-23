2948589b5123f115460e101b40f2dd51841ec2a1
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:11:52 +0530

    test: add testNoArgsConstructor to PatientProfileDTOTest

--- backend/src/test/java/com/hms/dto/PatientProfileDTOTest.java
+++ backend/src/test/java/com/hms/dto/PatientProfileDTOTest.java
@@ -5,4 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PatientProfileDTOTest {
 
+    @Test
+    public void testNoArgsConstructor() {
+        PatientProfileDTO dto = new PatientProfileDTO();
+        assertNull(dto.getFname());
+        assertNull(dto.getLname());
+    }
+
 }
