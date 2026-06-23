579ab53d8544e080aceeb59f16d242ed1f986a34
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:13:33 +0530

    test: add setup and mocks for PatientRepositoryTest

---
+++ backend/src/test/java/com/hms/repository/PatientRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.hms.repository;
+
+import com.hms.entity.Patient;
+import org.junit.jupiter.api.Test;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class PatientRepositoryTest {
+
+}
