259eaba0d94e3e4237ffe1d1400f6c10402e23b5
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:13:27 +0530

    test: add setup and mocks for CustomUserDetailsTest

---
+++ backend/src/test/java/com/hms/config/CustomUserDetailsTest.java
@@ -0,0 +1,10 @@
+package com.hms.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.Collection;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomUserDetailsTest {
+
+}
