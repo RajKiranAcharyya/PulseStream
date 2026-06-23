67693c9f65d6615aa791558759e163d795b70063
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:15:49 +0530

    test: verify setter and getter for contact with value 90003 in ContactMessage

--- backend/src/test/java/com/hms/entity/ContactMessageTest.java
+++ backend/src/test/java/com/hms/entity/ContactMessageTest.java
@@ -115,4 +115,11 @@ public class ContactMessageTest {
         obj.setContact("90002");
         assertEquals("90002", obj.getContact());
     }
+
+    @Test
+    public void testSetContact_Variant3() {
+        ContactMessage obj = new ContactMessage();
+        obj.setContact("90003");
+        assertEquals("90003", obj.getContact());
+    }
 }
