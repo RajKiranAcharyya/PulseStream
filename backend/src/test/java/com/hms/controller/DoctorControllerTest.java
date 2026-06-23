422a30cab503f96dd4b684c639de3c0a0d3e01d9
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:09:23 +0530

    test: add setup and mocks for DoctorControllerTest

---
+++ backend/src/test/java/com/hms/controller/DoctorControllerTest.java
@@ -0,0 +1,53 @@
+package com.hms.controller;
+
+import com.hms.dto.AvailabilityDTO;
+import com.hms.dto.DoctorProfileDTO;
+import com.hms.dto.PrescriptionDTO;
+import com.hms.entity.Availability;
+import com.hms.entity.Doctor;
+import com.hms.entity.Prescription;
+import com.hms.entity.Appointment;
+import com.hms.service.AppointmentService;
+import com.hms.service.AvailabilityService;
+import com.hms.service.DoctorService;
+import com.hms.service.PrescriptionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = DoctorController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+public class DoctorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AppointmentService appointmentService;
+
+    @MockBean
+    private PrescriptionService prescriptionService;
+
+    @MockBean
+    private AvailabilityService availabilityService;
+
+    @MockBean
+    private DoctorService doctorService;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+}
