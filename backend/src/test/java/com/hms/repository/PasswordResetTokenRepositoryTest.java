b168aa663f0cd6d96e97cc0ecee360ec69c97ec1
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:13:32 +0530

    test: add setup and mocks for PasswordResetTokenRepositoryTest

---
+++ backend/src/test/java/com/hms/repository/PasswordResetTokenRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.hms.repository;
+
+import com.hms.entity.PasswordResetToken;
+import org.junit.jupiter.api.Test;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class PasswordResetTokenRepositoryTest {
+
+}
