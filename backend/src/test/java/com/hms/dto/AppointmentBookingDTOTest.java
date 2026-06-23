2e033194214277ff121c96401ec5fa45466aa15b
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:16:09 +0530

    test: verify setter and getter for disease with value Cough in AppointmentBookingDTO

--- backend/src/test/java/com/hms/dto/AppointmentBookingDTOTest.java
+++ backend/src/test/java/com/hms/dto/AppointmentBookingDTOTest.java
@@ -91,4 +91,11 @@ public class AppointmentBookingDTOTest {
         obj.setDisease("Fever");
         assertEquals("Fever", obj.getDisease());
     }
+
+    @Test
+    public void testSetDisease_Variant2() {
+        AppointmentBookingDTO obj = new AppointmentBookingDTO();
+        obj.setDisease("Cough");
+        assertEquals("Cough", obj.getDisease());
+    }
 }
