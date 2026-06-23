032a613e192e2b1a48b985fa21ae5d6b55d81ce4
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:10:21 +0530

    test: add setup and mocks for DashboardControllerTest

---
+++ backend/src/test/java/com/hms/controller/DashboardControllerTest.java
@@ -0,0 +1,24 @@
+package com.hms.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = DashboardController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+public class DashboardControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+}
