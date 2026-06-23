230f69fdee97585c1b4fa4daad194982c1d37e1e
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:06:19 +0530

    test: add setup and mocks for EmailServiceTest

---
+++ backend/src/test/java/com/hms/service/EmailServiceTest.java
@@ -0,0 +1,26 @@
+package com.hms.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.*;
+
+public class EmailServiceTest {
+
+    @Mock
+    private JavaMailSender mailSender;
+
+    @InjectMocks
+    private EmailService emailService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+}
