2be731b5d674ae3678175bbc82c5aa290c1ca1b0
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-06-23 21:13:30 +0530

    test: add setup and mocks for AppointmentRepositoryTest

---
+++ backend/src/test/java/com/hms/repository/AppointmentRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.hms.repository;
+
+import com.hms.entity.Appointment;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class AppointmentRepositoryTest {
+
+}
